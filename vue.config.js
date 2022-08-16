80224183af8d2c900645eb5a7112f9371a67c297
JacksonHiew <nelson145258116@gmail.com>
2022-08-16 23:15:41 +0800

    Added global style config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "@/assets/style/style.scss";
+        `
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
