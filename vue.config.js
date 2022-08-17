e7d0865c77c95f26a4ceb946021ef67ffbfbe16d
JacksonHiew <nelson145258116@gmail.com>
2022-08-18 02:29:41 +0800

    Added https config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+const fs = require("fs");
 module.exports = defineConfig({
+  devServer: {
+    port: '8080',
+    server: {
+      type: "https",
+      options: {
+        key: fs.readFileSync('./server.key'),
+        cert: fs.readFileSync('./server.crt'),
+      },
+    },
+  },
   css: {
     loaderOptions: {
       scss: {
